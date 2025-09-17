20c28f3f878310e20eadb0306379662a88aba39e
Sultan <sultanbeimaganbet@gmail.com>
2025-09-17 10:46:35 +0500

    exercise1

---
+++ a1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void res(int n){
+    deque<int> dq;
+    int c = 1;
+
+    while(n){
+        int m = n % c;
+        // cout << m;
+
+        dq.push_front(n);
+        n--;
+
+        for(int i = 0; i < m; i++){
+            dq.push_front(dq.back());
+            dq.pop_back();
+        }
+        c++;
+    }
+    
+    for(int i : dq){
+        cout << i << " ";
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    while(n){
+        int m;
+        cin >> m;
+        res(m);
+        n--;
+        cout << endl;
+    }
+
+
+    return 0;
+}
